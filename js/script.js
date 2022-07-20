6cd66de1ab977a3a3b406f5e4ba86e9cdc4f941b
RitaLing1021 <ritaling1021@gmail.com>
2022-07-21 03:05:55 +0800

    定位

--- js/script.js
+++ js/script.js
@@ -163,11 +163,11 @@ jQuery("#prod").owlCarousel({
         if (settings.sticky === true) cssmenu.css('position', 'fixed');
 
         resizeFix = function() {
-          if ($( window ).width() > 1024) {
+          if ($( window ).width() > 768) {
             cssmenu.find('ul').show();
           }
 
-          if ($(window).width() <= 1024) {
+          if ($(window).width() <= 768) {
             cssmenu.find('ul').hide().removeClass('open');
           }
         };
